11850ad542f1ee0a77ef35239ce33eaaded1f7e9
Bryson Lee-Kwen <brysonleekwen@gmail.com>
2025-07-07 01:04:30 -0400

    main com

--- main.py
+++ main.py
@@ -2,6 +2,17 @@
 # Run indexer (Collect fingerprints, and index)
 # Initialize LLM conversation terminal (Create a tool to end conversation as well as allow user to save)
 # Save conversation and index it.
+
+
+# TODO: Connect tools to LLM for getting data.
+# TODO: Create a way for the LLM to focus more on the most recent (same day) entries and work with that (should be a simply metadata filter)
+# TODO: Add conversation to RAG
+# TODO: Multi-Modal Media
+# TODO: Empty conversation starter, end call tool
+# TODO: Medical Knowledge?
+# TODO: Setup scripts (to install ollama and make sure libraries are installed)
+# TODO: Change README so anyone can run and customize
+
 import brain_dump
 
 
